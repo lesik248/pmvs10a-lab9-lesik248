f97cce48126ed0196ffebb06bf250a4fc630927a
lesik248 <alesya.yarmosh24@gmail.com>
2026-05-19 16:32:43 +0300

    fix2

--- task1/composeApp/src/desktopTest/kotlin/com/example/weather/WeatherIntegrationTest.kt
+++ task1/composeApp/src/desktopTest/kotlin/com/example/weather/WeatherIntegrationTest.kt
@@ -13,6 +13,7 @@ import io.ktor.utils.io.ByteReadChannel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
